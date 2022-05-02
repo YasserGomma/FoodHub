b65496e7cc8a139136311352b5ddbb3b5dbe026b
Yasser <yassergomma204@gmail.com>
2022-05-02 02:26:18 +0200

    second commit

---
+++ app/src/main/java/com/example/foodhub/welcome_screen.java
@@ -0,0 +1,14 @@
+package com.example.foodhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class welcome_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
