77cac49045f6a6c2e2a3c97d24363c8ed0024435
Yasser <yassergomma204@gmail.com>
2022-05-28 13:29:25 +0200

    IX commit

--- app/src/main/java/com/example/foodhub/views/pages/b_account/PhoneRegistration.java
+++ app/src/main/java/com/example/foodhub/views/pages/b_account/PhoneRegistration.java
@@ -2,6 +2,7 @@ package com.example.foodhub.views.pages.b_account;
 
 import android.os.Bundle;
 import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 import com.example.foodhub.R;
 import com.example.foodhub.interfaces.EndPoints;
 import com.example.foodhub.views.networking.RetrofitCreation;
+import com.example.foodhub.views.pages.c_home.Home;
 import com.example.foodhub.views.pages.parents.BaseActivity;
 
 import okhttp3.RequestBody;
@@ -37,11 +39,15 @@ public class PhoneRegistration extends BaseActivity {
                     @Override
                     public void onResponse(Call<RequestBody> call, Response<RequestBody> response) {
                         Toast.makeText(PhoneRegistration.this, "Check your phone messages", Toast.LENGTH_LONG).show();
+                        go_screen(PhoneRegistration.this, Home.class);
                     }
 
                     @Override
                     public void onFailure(Call<RequestBody> call, Throwable t) {
-                        Toast.makeText(PhoneRegistration.this, "Please try again", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(PhoneRegistration.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("error",t.getMessage());
+                        go_screen(PhoneRegistration.this, Home.class);
+
 
                     }
                 });
